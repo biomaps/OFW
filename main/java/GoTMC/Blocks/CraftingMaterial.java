e7d3cfe009694e7ef0c0cf5e7466ebc5f2740899
deathmuffin77 <40216627+deathmuffin77@users.noreply.github.com>
2018-10-05 13:55:05 -0400

    Crafting material Class 10.5.18

---
+++ main/java/GoTMC/Blocks/CraftingMaterial.java
@@ -0,0 +1,30 @@
+package GoTMC.Blocks;
+
+import net.minecraft.item.Item;
+import net.minecraft.potion.PotionHelper;
+
+public class ItemNewBlock extends Item{
+	public ItemNewBlock() {
+		
+		//this.setFull3D();
+		//held like a sword
+		
+		this.setNoRepair();
+		//cannot combine em
+		
+		//this.setContainerItem(
+		//gives back () item after being crafted with
+		
+		this.setMaxStackSize(64);
+		//yea
+		
+//		this.setMaxDamage(100);
+		//eh
+		
+		//this.setPotionEffect(PotionHelper.ghastTearEffect);
+		//can make regen pots
+		
+		
+		
+	}
+}
