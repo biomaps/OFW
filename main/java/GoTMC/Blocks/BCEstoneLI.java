1d2fe756c40143f14099e57e8b7803f737973e97
deathmuffin77 <40216627+deathmuffin77@users.noreply.github.com>
2018-10-26 14:13:50 -0400

    Stone class
    for dropz ⛏

---
+++ main/java/GoTMC/Blocks/BCEstoneLI.java
@@ -0,0 +1,20 @@
+package GoTMC.Blocks;
+
+import net.minecraft.block.material.Material;
+
+public class BCEstone extends BCEblock{
+
+	public BCEstone(Material material) {
+		super(material);
+	}
+  public Item getItemDropped(int metadata, Random rand, int fortune){
+		return IanMod.itemBlueberry;
+	
+	}
+
+	public int quantityDropped(Random rand){
+		return 3 + rand.nextInt(4);
+	
+	}
+
+}
