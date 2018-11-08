6cd0ae150fc799d4078576e67d463935c3e8b75c
deathmuffin77 <40216627+deathmuffin77@users.noreply.github.com>
2018-11-08 16:51:32 -0500

    ok

---
+++ main/java/GoTMC/Blocks/BCEblock.java
@@ -0,0 +1,14 @@
+package GoTMC.Blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BCEblock extends Block{
+
+	public BCEblock(Material material) {	
+		super(material);
+		
+	}
+	
+
+}
---
+++ main/java/GoTMC/Blocks/BCEbrick.java
@@ -0,0 +1,12 @@
+package GoTMC.Blocks;
+
+import net.minecraft.block.material.Material;
+
+public class BCEbrick extends BCEblock{
+
+	public BCEbrick(Material material) {
+		super(material);
+		// TODO Auto-generated constructor stub
+	}
+
+}
