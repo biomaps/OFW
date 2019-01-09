3f22d5305da3bf57836367d4597c324b34cb7bd3
deathmuffin77 <40216627+deathmuffin77@users.noreply.github.com>
2019-01-09 14:16:09 -0500

    registering mobs 5/10 1.9.19
    🆗 ❓

--- main/java/GoTMC/Mobs/EntitySheep1.java
+++ main/java/GoTMC/Mobs/EntitySheep1.java
@@ -20,14 +20,43 @@ public class EntitySheep1 {
 	public static void mainRegistry(){ 
 
 
-		//registerEntity(); 
+		registerEntityDW(); 
+		
+		registerEntityA1();
+		registerEntityA2();
+		registerEntityA3();
+		registerEntityA4();
+		registerEntityA5();
+		
+		registerEntityE1();
+		registerEntityE2();
+		registerEntityE3();
+		registerEntityE4();
+		registerEntityE5();
+		
 		
 
 
 	} 
 	
-	public static void registerEntity(){
-		//createEntity(EntityDireWolfMob.class, "Dire Wolf", 0xF5C15F, 0xFC2121); 
+	public static void registerEntityDW(){
+		createEntity(EntityDireWolfMob.class, "Dire Wolf", 0xF5C15F, 0xFC2121); 
+	}
+	
+	public static void registerEntityA1(){
+		createEntity(EntityDireWolfMob.class, "Arabian", 0xc49b71 , 0x585858); 
+	} 
+	public static void registerEntityA2(){
+		createEntity(EntityDireWolfMob.class, "Moslem Soldier", 0x357648, 0xfff); 
+	} 
+	public static void registerEntityA3(){
+		createEntity(EntityDireWolfMob.class, "Moslem Archer", 0x357648, 0xfff); 
+	} 
+	public static void registerEntityA4(){
+		createEntity(EntityDireWolfMob.class, "Jihad Warlord", 0x357648, 0xfff); 
+	} 
+	public static void registerEntityA5(){
+		createEntity(EntityDireWolfMob.class, "Jihad Marksman", 0x357648, 0xfff); 
 	} 
 	
 	public static void createEntity(Class entityClass, String entityName, int solidColor, int spotColor){ 
