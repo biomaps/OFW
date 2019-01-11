a05f638145ece98d00d743ee2d7c5817ec3e2d17
deathmuffin77 <40216627+deathmuffin77@users.noreply.github.com>
2019-01-11 14:17:29 -0500

    Preset mob class 1.11.19
    🇺🇸 😄

---
+++ main/java/GoTMC/Mobs/EntityDireWolfMob.java
@@ -0,0 +1,40 @@
+package com.ianRJ.ianMCmod.mobStuffz;
+//soldiers
+import com.ianRJ.ianMCmod.IanMod;
+
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIFollowOwner;
+import net.minecraft.entity.ai.EntityAIMoveIndoors;
+import net.minecraft.entity.ai.EntityAIOpenDoor;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityTameable;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class EntityPersonMobAggressiveL extends EntityTameable{
+
+	public EntityPersonMobAggressiveL(World par1world) {
+		super(par1world);
+		this.setSize(1.8F, 0.9F);
+		this.tasks.addTask(0,new EntityAIWander(this, 0.4D));
+		this.tasks.addTask(2, new EntityAISwimming(this));
+		this.tasks.addTask(3, new EntityAIOpenDoor(this, true));
+		
+		
+	} 
+	public boolean isAIEnabled(){
+		return true;
+	}
+	@Override
+	public EntityAgeable createChild(EntityAgeable p_90011_1_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
