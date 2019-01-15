c2d26912459ef7f61ed70d198c176ae770fff8da
deathmuffin77 <40216627+deathmuffin77@users.noreply.github.com>
2019-01-15 13:54:37 -0500

    More mob Updates
    yessir
     🥚 vs 💁 
         🥚 wins

--- main/java/GoTMC/Mobs/EntitySheep1.java
+++ main/java/GoTMC/Mobs/EntitySheep1.java
@@ -28,13 +28,13 @@ public class EntitySheep1 {
 		registerEntityA4();
 		registerEntityA5();
 		
-		/*
+		
 		registerEntityE1(); 
 		registerEntityE2();
 		registerEntityE3();
 		registerEntityE4();
 		registerEntityE5();
-		*/
+		
 		
 
 
@@ -44,21 +44,41 @@ public class EntitySheep1 {
 		createEntity(EntityDireWolfMob.class, "Dire Wolf", 0xF5C15F, 0xFC2121); 
 	}
 	
+	/*MOSLEM*/
+	
 	public static void registerEntityA1(){
 		createEntity(EntityDireWolfMob.class, "Arabian", 0xc49b71 , 0x585858); 
 	} 
 	public static void registerEntityA2(){
-		createEntity(EntityDireWolfMob.class, "Moslem Soldier", 0x357648, 0xfff); 
+		createEntity(EntityDireWolfMob.class, "Moslem Soldier", 0x357648, 0xfffff); 
 	} 
 	public static void registerEntityA3(){
-		createEntity(EntityDireWolfMob.class, "Moslem Archer", 0x357648, 0xfff); 
+		createEntity(EntityDireWolfMob.class, "Moslem Archer", 0x357648, 0xfffff); 
 	} 
 	public static void registerEntityA4(){
-		createEntity(EntityDireWolfMob.class, "Jihad Warlord", 0x357648, 0xfff); 
+		createEntity(EntityDireWolfMob.class, "Jihad Warlord", 0x357648, 0xfffff); 
 	} 
 	public static void registerEntityA5(){
-		createEntity(EntityDireWolfMob.class, "Jihad Marksman", 0x357648, 0xfff); 
+		createEntity(EntityDireWolfMob.class, "Jihad Marksman", 0x357648, 0xfffff); 
 	} 
+	/* EUROPEAN*/
+	
+	public static void registerEntityE1(){
+		createEntity(EntityDireWolfMob.class, "European", 0xfdd2a6, 0x585858); 
+	} 
+	public static void registerEntityE2(){
+		createEntity(EntityDireWolfMob.class, "Norman Crusader", 0xffffff, 0x10026); 
+	} 
+	public static void registerEntityE3(){
+		createEntity(EntityDireWolfMob.class, "Norman Crusader Archer", 0xffffff, 0x10026); 
+	} 
+	public static void registerEntityE4(){
+		createEntity(EntityDireWolfMob.class, "Crusader Knight", 0xffffff, 0x10026); 
+	} 
+	public static void registerEntityE5(){
+		createEntity(EntityDireWolfMob.class, "Crusader Marksman", 0xffffff, 0x10026); 
+	} 
+	
 	
 	public static void createEntity(Class entityClass, String entityName, int solidColor, int spotColor){ 
 		int randomId = EntityRegistry.findGlobalUniqueEntityId(); 
